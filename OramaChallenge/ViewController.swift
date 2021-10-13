d3bf9a3a1b1cf3a904c334ce497b7f1cb3507288
Carlos Roberto Modinez Junior <modinezcarlos@NBBV033178.local>
2021-10-13 20:41:07 -0300

    ORAMA-001 - Cria novo projeto vazio

---
+++ OramaChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OramaChallenge
+//
+//  Created by Carlos Roberto Modinez Junior on 13/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
